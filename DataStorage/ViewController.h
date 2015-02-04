40a92e5120f4f5425fa5fac26fd3cd081c7c0563
jamalping <silvercreek@Ryans-MBA.local>
2015-02-04 10:57:30 +0800

    Initial Commit

---
+++ DataStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataStorage
+//
+//  Created by jamalping on 15-2-4.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataStorage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DataStorage
+//
+//  Created by jamalping on 15-2-4.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
